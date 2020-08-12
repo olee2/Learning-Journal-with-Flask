fc80e082c5187c774533272894777d0fb9bf5237
olee2 <65301179+olee2@users.noreply.github.com>
2020-08-12 11:31:54 +0200

    Create models.py

---
+++ models.py
@@ -0,0 +1,24 @@
+import datetime
+
+from peewee import *
+
+DATABASE = SqliteDatabase("journal.db")
+
+class Entry(Model):
+
+    title = CharField(unique=True)
+    date = DateTimeField(default=datetime.datetime.now)
+    time_spent = IntegerField()
+    learned = TextField()
+    resources = TextField()
+    
+
+    class Meta:
+        database = DATABASE
+        order_by = ("-timestamp",)
+
+   
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([Entry], safe=True)
+    
