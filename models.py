b4c8cff2131825fa414e3927a134edb8069c0bf0
olee2 <65301179+olee2@users.noreply.github.com>
2020-08-28 12:18:31 +0200

    Update models.py

--- models.py
+++ models.py
@@ -5,20 +5,20 @@ from peewee import *
 DATABASE = SqliteDatabase("journal.db")
 
 class Entry(Model):
-
     title = CharField(unique=True)
     date = DateTimeField(default=datetime.datetime.now)
     time_spent = IntegerField()
     learned = TextField()
     resources = TextField()
     
-
     class Meta:
         database = DATABASE
         order_by = ("-timestamp",)
 
-   
+
+
 def initialize():
     DATABASE.connect()
     DATABASE.create_tables([Entry], safe=True)
+    DATABASE.close()
     
